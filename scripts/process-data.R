5909932c9502cc6ce2805e09277177e08ca2b5fc
nicholas-camarda <ncamarda93@gmail.com>
2021-06-22 20:44:36 -0400

    comm

--- scripts/process-data.R
+++ scripts/process-data.R
@@ -7,8 +7,9 @@ if(length(new_packages)) install.packages(new_packages)
 library(tidyverse)
 
 #' @param data_dir the parent directory of your data, e.g. my_data/
-#' @param project_dir the subdirectory containing your csv tailcuff files under *data_dir*, e.g. sor-pilot3/
-process_data_fn <- function(data_dir, project_dir){
+#' @note underneath *data_dir* should simply be .xlsx files
+#' @output writes a processed data file to data/ directory
+process_data_fn <- function(data_dir){
   raw_data_dir_full <- file.path(data_dir, project_dir)
   raw_data_tbl <- tibble(fn = list.files(raw_data_dir_full, full.names = TRUE)) %>%
     mutate(Date = str_split(string = fn, pattern = "-a_|-b_|\\.", simplify = TRUE)[,2]) %>%
@@ -24,6 +25,6 @@ process_data_fn <- function(data_dir, project_dir){
   write_csv(raw_data_tbl, "~/Downloads/temp.csv")
 }
 
-
-
+# move your dat
+data_dir <- "data/"
 process_data_fn()
